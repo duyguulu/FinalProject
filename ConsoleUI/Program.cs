fbe14aad5c91df22e22d0193ef12b804e7333192
duyguulu <d.ulu.duygu@gmail.com>
2021-02-16 10:14:58 +0300

    9. gün commit 2. mola

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,6 +9,18 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
+			//ProductTest();
+
+			CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+			foreach (var category in categoryManager.GetAll())
+			{
+				Console.WriteLine(category.CategoryName);
+			}
+
+		}
+
+		private static void ProductTest()
+		{
 			ProductManager productManager = new ProductManager(new EfProductDal());
 			//foreach (var product in productManager.GetAll())
 			//foreach (var product in productManager.GetByUnitPrice(40,100))
@@ -16,7 +28,6 @@ namespace ConsoleUI
 			{
 				Console.WriteLine(product.ProductName);
 			}
-			
 		}
 	}
 }
---
+++ Entities/Concrete/Order.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+	public class Order:IEntity
+	{
+		public int OrderId { get; set; }
+		public string CustomerId { get; set; } //normalize değil. bu yuzden string
+		public int EmployeeId { get; set; }
+		public DateTime OrderDate { get; set; }
+		public string ShipCity { get; set; }
+	}
+}
