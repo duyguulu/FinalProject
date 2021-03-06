8f955edab6b745874c1aa351c0081b5cf966c9a2
duyguulu <d.ulu.duygu@gmail.com>
2021-03-06 13:09:21 +0300

    14. gün hata duzeltildi

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,5 +1,6 @@
 using Business.Abstract;
 using Business.Concrete;
+using Core.Utilities.IoC;
 using Core.Utilities.Security._JWT;
 using Core.Utilities.Security.Encryption_;
 using DataAccess.Abstract;
@@ -7,6 +8,7 @@ using DataAccess.Concrete.EntityFramework;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -39,6 +41,8 @@ namespace WebAPI
 			//services.AddSingleton<IProductService,ProductManager>();
 			//services.AddSingleton<IProductDal, EfProductDal>();
 
+			services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
 			var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
 			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -55,8 +59,7 @@ namespace WebAPI
 						IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
 					};
 				});
-			//burayý calýstýrýnca duzelecekmiþ
-			//ServiceTool.Create(services);
+			ServiceTool.Create(services);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
